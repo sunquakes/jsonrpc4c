b74bef0c03fdcc5e3b50f654c7d1c81ab91196b8
Shing <sunquakes@outlook.com>
2023-07-23 11:19:39 +0800

    Call object method using reflect.

--- test/boost_test.cc
+++ test/boost_test.cc
@@ -10,12 +10,28 @@ bool FoundObjectMethod(C &c, std::string method) {
     bool found = false;
 
     boost::mp11::mp_for_each<Fd>([&](auto D) {
-        if( !found && method == D.name)
-        {
+        if (!found && method == D.name) {
             found = true;
         }
     });
     return found;
+}
+
+template<class C, typename R, typename... A>
+void *CallObjectMethod(C &c, std::string method, A... args) {
+    using Fd = boost::describe::describe_members<C,
+            boost::describe::mod_public | boost::describe::mod_function>;
+
+    void *result;
+
+    boost::mp11::mp_for_each<Fd>([&](auto D) {
+        if (method == D.name) {
+            R (C::*pmf)(A...) = D.pointer;
+            auto res = (c.*pmf)(args...);
+            result = &res;
+        }
+    });
+    return result;
 }
 
 struct Object {
@@ -30,4 +46,10 @@ TEST(BoostTestSuite, TestFoundObjectMethod) {
     Object obj;
     ASSERT_TRUE(FoundObjectMethod<Object>(obj, "add"));
     ASSERT_FALSE(FoundObjectMethod<Object>(obj, "greet"));
+}
+
+TEST(BoostTestSuite, TestCallObjectMethod) {
+    Object obj;
+    int result = *(reinterpret_cast<int *>(CallObjectMethod<Object, int, int>(obj, "add", 1, 1)));
+    ASSERT_EQ(result, 2);
 }
