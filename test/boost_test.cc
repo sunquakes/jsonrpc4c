5424e53069e96c3093a8558aef93f0b002432113
Shing <sunquakes@outlook.com>
2023-07-24 21:09:28 +0800

    Using reflection call object method.

--- test/boost_test.cc
+++ test/boost_test.cc
@@ -1,9 +1,53 @@
 #include <gtest/gtest.h>
 #include <boost/describe.hpp>
 #include <boost/mp11.hpp>
+#include <boost/json/src.hpp>
+#include <boost/type_traits.hpp>
+#include <boost/utility/string_view.hpp>
+#include <stdexcept>
+#include <string>
+
+template<class C1, class C2, class R, class... A, std::size_t... I>
+boost::json::value
+CallImpl_(C1 &c1, R (C2::*pmf)(A...), boost::json::array const &args,
+          std::index_sequence<I...>) {
+    return boost::json::value_from((c1.*pmf)(boost::json::value_to<boost::remove_cv_ref_t<A> >(args[I])...));
+}
+
+template<class C1, class C2, class R, class... A>
+boost::json::value
+CallImpl(C1 &c1, R (C2::*pmf)(A...), boost::json::array const &args) {
+    if (args.size() != sizeof...(A)) {
+        throw std::invalid_argument("Invalid number of arguments");
+    }
+
+    return CallImpl_(c1, pmf, args, std::index_sequence_for<A...>());
+}
 
 template<class C>
-bool FoundObjectMethod(C &c, std::string method) {
+boost::json::value
+Call(C &c, std::string method, boost::json::value const &args) {
+    using Fd = boost::describe::describe_members<C, boost::describe::mod_public | boost::describe::mod_function>;
+
+    bool found = false;
+    boost::json::value result;
+
+    boost::mp11::mp_for_each<Fd>([&](auto D) {
+        if (!found && method == D.name) {
+            result = CallImpl(c, D.pointer, args.as_array());
+            found = true;
+        }
+    });
+
+    if (!found) {
+        throw std::invalid_argument("Invalid method name");
+    }
+
+    return result;
+}
+
+template<class C>
+bool Found(C &c, std::string method) {
     using Fd = boost::describe::describe_members<C,
             boost::describe::mod_public | boost::describe::mod_function>;
 
@@ -17,39 +61,27 @@ bool FoundObjectMethod(C &c, std::string method) {
     return found;
 }
 
-template<class C, typename... A>
-void *CallObjectMethod(C &c, std::string method, A... args) {
-    using Fd = boost::describe::describe_members<C,
-            boost::describe::mod_public | boost::describe::mod_function>;
-
-    void *result;
-
-    boost::mp11::mp_for_each<Fd>([&](auto D) {
-        if (method == D.name) {
-            auto (C::*pmf)(A...) = D.pointer;
-            auto res = (c.*pmf)(args...);
-            result = &res;
-        }
-    });
-    return result;
-}
-
 struct Object {
-    int add(int x, int y) {
+    std::string Greet(std::string const &who) {
+        return "Hello, " + who + "!";
+    }
+
+    int Add(int x, int y) {
         return x + y;
     }
 };
 
-BOOST_DESCRIBE_STRUCT(Object, (), (add))
+BOOST_DESCRIBE_STRUCT(Object, (), (Greet, Add))
+
 
 TEST(BoostTestSuite, TestFoundObjectMethod) {
     Object obj;
-    ASSERT_TRUE(FoundObjectMethod<Object>(obj, "add"));
-    ASSERT_FALSE(FoundObjectMethod<Object>(obj, "greet"));
+    ASSERT_TRUE(Found<Object>(obj, "Add"));
+    ASSERT_FALSE(Found<Object>(obj, "Sub"));
 }
 
 TEST(BoostTestSuite, TestCallObjectMethod) {
     Object obj;
-    int result = *(reinterpret_cast<int *>(CallObjectMethod(obj, "add", 1, 1)));
-    ASSERT_EQ(result, 2);
+    ASSERT_EQ(Call(obj, "Greet", {"World"}), "Hello, World!");
+    ASSERT_EQ(Call(obj, "Add", {1, 2}), 3);
 }
